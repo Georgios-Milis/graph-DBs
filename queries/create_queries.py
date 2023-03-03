edab8daff9c6771e6a99cfed925bf9f53f3198c3
Georgios Gkrinias <el18060@mail.ntua.gr>
2023-03-04 01:27:36 +0200

    Replaced CREATE by MERGE in create queries.

--- queries/create_queries.py
+++ queries/create_queries.py
@@ -29,7 +29,7 @@ def create_papers(self, papers):
         query = (
             """
             UNWIND $papers AS map
-            CREATE UNIQUE (p:Paper)
+            MERGE (p:Paper)
             SET p = map
             """
         )
@@ -47,7 +47,7 @@ def create_authors(self, authors):
         query = (
             """
             UNWIND $authors AS map
-            CREATE UNIQUE (a:Author)
+            MERGE (a:Author)
             SET a = map
             """
         )
@@ -115,7 +115,7 @@ def create_references(self, references):
             UNWIND $references AS edge
             MATCH (p1:Paper), (p2:Paper)
             WHERE p1.id = edge.from AND p2.id = edge.to
-            CREATE UNIQUE (p1)-[r:REFERENCE]->(p2)
+            MERGE (p1)-[r:REFERENCE]->(p2)
             """  
         )
         tx.run(query, references=references)
@@ -159,7 +159,7 @@ def create_authorships(self, authorships):
             UNWIND $authorships AS edge
             MATCH (a:Author), (p:Paper)
             WHERE a.id = edge.author AND p.id = edge.paper
-            CREATE UNIQUE (a)-[r:AUTHORSHIP]->(p)
+            MERGE (a)-[r:AUTHORSHIP]->(p)
             """  
         )
         tx.run(query, authorships=authorships)

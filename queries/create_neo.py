d0fa3c1fa6d25e38985a5a1972b5e3b4c871fd29
Georgios Gkrinias <el18060@mail.ntua.gr>
2023-03-09 04:08:03 +0200

    Refinements in crud scripts and queries.

--- queries/create_neo.py
+++ queries/create_neo.py
@@ -2,77 +2,19 @@
 import logging
 from neo4j.exceptions import ServiceUnavailable
 
-def create_people(self, papers):
-    """
-    Insert all papers to database.
-    """
-    def create_people_tx(tx, papers):
-        query = (
-            """
-            UNWIND $papers AS params
-            CREATE (n:Paper)
-            SET n += params
-            """
-        )
-        tx.run(query, papers=papers)
 
+def paper_constraints(self):
+    def paper_constraints_tx(tx):
+        tx.run("CREATE CONSTRAINT FOR (n:Paper) REQUIRE n.id IS UNIQUE")
     with self.driver.session(database=self.instance) as session:
-        session.execute_write(create_people_tx, papers)
+        session.execute_write(paper_constraints_tx)
 
 
-def create_references(self, references):
-    """
-    Insert all references to database.
-    """
-    def create_references_tx(tx, references):
-        query = (
-            """
-            UNWIND $references AS edge
-            MATCH (p1:Paper {id: edge.from}), (p2:Paper {id: edge.to})
-            CREATE (p1)-[r:REFERENCE]->(p2)
-            """
-        )
-        tx.run(query, references=references)
-
+def author_constraints(self):
+    def author_constraints_tx(tx):
+        tx.run("CREATE CONSTRAINT FOR (n:Author) REQUIRE n.id IS UNIQUE")
     with self.driver.session(database=self.instance) as session:
-        session.execute_write(create_references_tx, references)
-
-def create_authorships(self, authorships):
-    """
-    Insert all references to database.
-    """
-    def create_authorships_tx(tx, authorships):
-        query = (
-            """
-            UNWIND $authorships AS edge
-            MATCH (a:Author {id: edge.author}), (p:Paper {id: edge.paper})
-            CREATE (a)-[r:AUTHORSHIP]->(p)
-            """
-        )
-        tx.run(query, authorships=authorships)
-
-    with self.driver.session(database=self.instance) as session:
-        session.execute_write(create_authorships_tx, authorships)
-    
-
-def create_paper(self, attributes):
-    """
-    Create and return a paper.
-    """
-    def create_paper_tx(tx, attributes):
-        query = (
-            """
-            WITH $attributes AS params
-            CREATE (p:Paper {id: params.id})
-            ON CREATE SET p = properties(params)
-            """
-        )
-        result = tx.run(query, attributes=attributes)
-        return result
-
-    with self.driver.session(database=self.instance) as session:
-        result = session.execute_write(create_paper_tx, attributes)
-        return result
+        session.execute_write(author_constraints_tx)
 
 
 def create_papers(self, papers):
@@ -83,8 +25,8 @@ def create_papers(self, papers):
         query = (
             """
             UNWIND $papers AS params
-            CREATE (p:Paper)
-            SET p += params
+            CREATE (n:Paper)
+            SET n += params
             """
         )
         tx.run(query, papers=papers)
@@ -101,8 +43,8 @@ def create_authors(self, authors):
         query = (
             """
             UNWIND $authors AS params
-            CREATE (a:Author)
-            SET a += params
+            CREATE (n:Author)
+            SET n += params
             """
         )
         tx.run(query, authors=authors)
@@ -111,28 +53,62 @@ def create_authors(self, authors):
         session.execute_write(create_authors_tx, authors)
 
 
+def create_references(self, references):
+    """
+    Insert all references to database.
+    """
+    def create_references_tx(tx, references):
+        query = (
+            """
+            UNWIND $references AS edge
+            MATCH (p1:Paper {id: edge.from}), (p2:Paper {id: edge.to})
+            MERGE (p1)-[r:REFERENCE]->(p2)
+            """
+        )
+        tx.run(query, references=references)
+
+    with self.driver.session(database=self.instance) as session:
+        session.execute_write(create_references_tx, references)
+
+
+def create_authorships(self, authorships):
+    """
+    Insert all references to database.
+    """
+    def create_authorships_tx(tx, authorships):
+        query = (
+            """
+            UNWIND $authorships AS edge
+            MATCH (a:Author {id: edge.author}), (p:Paper {id: edge.paper})
+            MERGE (a)-[r:AUTHORSHIP]->(p)
+            """
+        )
+        tx.run(query, authorships=authorships)
+
+    with self.driver.session(database=self.instance) as session:
+        session.execute_write(create_authorships_tx, authorships)
+
+
+def create_paper(self, attributes):
+    """
+    Create and return a paper.
+    """
+    def create_paper_tx(tx, attributes):
+        tx.run("CREATE (:Paper $attributes)", attributes=attributes)
+
+    with self.driver.session(database=self.instance) as session:
+        session.execute_write(create_paper_tx, attributes)
+
+
 def create_author(self, attributes):
     """
     Create and return an author.
     """
     def create_author_tx(tx, attributes):
-        query = (
-            """
-            WITH $attributes AS params
-            MERGE (a:Author {id: params.id})
-            ON CREATE SET a = properties(params)
-            """
-        )
-        result = tx.run(query, attributes=attributes)
-        try:
-            return [{'a': row['a']['id']} for row in result]
-        except ServiceUnavailable as exception:
-            logging.error(f"{query} raised an error: \n {exception}")
-            raise
+        tx.run("CREATE (:Author $attributes)", attributes=attributes)
 
     with self.driver.session(database=self.instance) as session:
-        result = session.execute_write(create_author_tx, attributes)
-        return result
+        session.execute_write(create_author_tx, attributes)
 
 
 def create_reference(self, id, ref_id):
@@ -160,25 +136,6 @@ def create_reference(self, id, ref_id):
         return result
 
 
-# def create_references(self, references):
-#     """
-#     Insert all references to database.
-#     """
-#     def create_references_tx(tx, references):
-#         query = (
-#             """
-#             UNWIND $references AS edge
-#             MATCH (p1:Paper), (p2:Paper)
-#             WHERE p1.id = edge.from AND p2.id = edge.to
-#             MERGE (p1)-[r:REFERENCE]->(p2)
-#             """
-#         )
-#         tx.run(query, references=references)
-
-#     with self.driver.session(database=self.instance) as session:
-#         session.execute_write(create_references_tx, references)
-
-
 def create_authorship(self, author_id, paper_id):
     """
     Create an authorship.
@@ -202,36 +159,3 @@ def create_authorship(self, author_id, paper_id):
     with self.driver.session(database=self.instance) as session:
         result = session.execute_write(create_authorship_tx, author_id, paper_id)
         return result
-
-
-# def create_authorships(self, authorships):
-#     """
-#     Insert all references to database.
-#     """
-#     def create_authorships_tx(tx, authorships):
-#         query = (
-#             """
-#             UNWIND $authorships AS edge
-#             MATCH (a:Author), (p:Paper)
-#             WHERE a.id = edge.author AND p.id = edge.paper
-#             MERGE (a)-[r:AUTHORSHIP]->(p)
-#             """
-#         )
-#         tx.run(query, authorships=authorships)
-
-#     with self.driver.session(database=self.instance) as session:
-#         session.execute_write(create_authorships_tx, authorships)
-
-
-def paper_constraints(self):
-    def paper_constraints_tx(tx):
-        tx.run("CREATE CONSTRAINT FOR (n:Paper) REQUIRE n.id IS UNIQUE")
-    with self.driver.session(database=self.instance) as session:
-        session.execute_write(paper_constraints_tx)
-
-
-def author_constraints(self):
-    def author_constraints_tx(tx):
-        tx.run("CREATE CONSTRAINT FOR (n:Author) REQUIRE n.id IS UNIQUE")
-    with self.driver.session(database=self.instance) as session:
-        session.execute_write(author_constraints_tx)
